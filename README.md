ed694cc69f2c7bf4350ca8062467aff0f68fb1d2
Lucas Nogueira <lucas@tauri.studio>
2020-11-27 16:19:13 -0300

    feat(project): add wallet CLI cargo project

---
+++ README.md
@@ -0,0 +1,127 @@
+# IOTA Wallet CLI
+
+Command line interface application for the [IOTA wallet library](https://github.com/iotaledger/wallet.rs).
+
+## Usage
+
+After downloading the CLI, create a new account:
+
+```
+$ ./wallet new --node http://node.url:port --alias ALIAS
+```
+
+If you already created an account, you can just run the CLI without args to get to the account selector:
+
+```
+$ ./wallet
+```
+
+Alternatively, you can select the account to use with the `account` command:
+
+```
+$ ./wallet account "my first account"
+```
+
+## Commands
+
+The wallet CLI has a set of main commands accesible with `$ ./wallet COMMAND [ARGS]` and a dedicated command list for the account prompt.
+
+### Main commands
+
+#### help [COMMAND]
+
+Prints the CLI help information. If a command is specified, the command's help will be printed.
+
+#### new --node "http://node.url:portNumber" [--alias ALIAS --mnemonic 24_WORD_MNEMONIC_PHRASE]
+
+Creates a new account connecting to the specified node URL. Optionally takes the account alias and 24 word mnemonic phrase.
+
+#### account ALIAS
+
+Selects the account associated with the specified alias.
+
+#### delete ALIAS
+
+Deletes the account associated with the specified alias.
+
+#### sync
+
+Synchronizes all accounts with the Tangle.
+
+#### backup PATH
+
+Backups the wallet database to the specified path.
+
+#### import PATH
+
+Imports the accounts stored on the specified backup path.
+
+### Account prompt commands
+
+#### help [COMMAND]
+
+Prints the CLI help information. If a command is specified, the command's help will be printed.
+
+#### exit
+
+Exits the account prompt.
+
+#### sync
+
+Synchronizes the account with the Tangle.
+
+#### address
+
+Generates a new unused address.
+
+#### balance
+
+Gets the account balance.
+
+#### list-addresses
+
+Lists the account's addresses.
+
+#### list-messages [MESSAGE_ID] [--type TYPE]
+
+Lists the account's messages.
+If an id is specified, the query will look for the message associated with that id.
+If a type is specified, the messages will be filtered based on it.
+
+- Possible `type` values: "received, "sent", "failed", "unconfirmed" or "value"
+
+#### transfer ADDRESS AMOUNT
+
+Transfer funds from the account to the given Bech32 address.
+
+#### promote [MESSAGE_ID]
+
+Promotes the specified message.
+
+#### reattach [MESSAGE_ID]
+
+Reattaches the specified message.
+
+#### retry [MESSAGE_ID]
+
+Retries (promotes or reattaches) the specified message.
+
+## Caveats
+
+### Database path
+
+By default the database path is `./wallet-cli-database` but you can change this with the `WALLET_DATABASE_PATH` environment variable:
+
+```
+$ export WALLET_DATABASE_PATH=/path/to/database # or add it to your .bashrc, .zshrc
+$ ./wallet [COMMAND] [OPTIONS]
+```
+
+## Contributing
+
+To run the CLI from source, install Rust (usually through [Rustup](https://rustup.rs/)) and run the following commands:
+
+```
+$ git clone https://github.com/iotaledger/wallet.cli
+$ cargo run -- [COMMAND] [OPTIONS]
+```
---
+++ src/main.rs
@@ -0,0 +1,178 @@
+//! Wallet CLI
+//! Create a new account: `$ cargo run -- new --node http://localhost:14265`
+
+use clap::{load_yaml, App, AppSettings, ArgMatches};
+use console::Term;
+use dialoguer::{theme::ColorfulTheme, Select};
+use iota_wallet::{
+    account::Account, account_manager::AccountManager, client::ClientOptionsBuilder, Result,
+    WalletError,
+};
+use once_cell::sync::OnceCell;
+use tokio::runtime::Runtime;
+
+use std::{env::var_os, sync::Mutex};
+
+mod account;
+
+const CLI_TEMPLATE: &'static str = "\
+  {before-help}{bin} {version}\n\
+  {about-with-newline}\n\
+  {usage-heading}\n    {usage}\n\
+  \n\
+  {all-args}{after-help}\
+";
+
+const ACCOUNT_CLI_TEMPLATE: &'static str = "\
+  {all-args}{after-help}\
+";
+
+fn print_error(e: WalletError) {
+    println!("ERROR: {}", e.to_string());
+}
+
+static RUNTIME: OnceCell<Mutex<Runtime>> = OnceCell::new();
+
+pub fn block_on<C: futures::Future>(cb: C) -> C::Output {
+    let runtime = RUNTIME.get().unwrap();
+    runtime.lock().unwrap().block_on(cb)
+}
+
+fn pick_account(account_cli: &App<'_>, accounts: Vec<Account>) -> Result<()> {
+    let items: Vec<&String> = accounts.iter().map(|acc| acc.alias()).collect();
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select an account to manipulate")
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr())?;
+    if let Some(selected) = selection {
+        account::account_prompt(account_cli, accounts[selected].clone());
+        pick_account(account_cli, accounts)?;
+    }
+    Ok(())
+}
+
+fn select_account_command(account_cli: &App<'_>, manager: &AccountManager, matches: &ArgMatches) {
+    if let Some(matches) = matches.subcommand_matches("account") {
+        let alias = matches.value_of("alias").unwrap();
+        if let Some(account) = manager.get_account_by_alias(alias) {
+            account::account_prompt(account_cli, account);
+        } else {
+            println!("Account not found");
+        }
+    }
+}
+
+fn new_account_command(
+    account_cli: &App<'_>,
+    manager: &AccountManager,
+    matches: &ArgMatches,
+) -> Result<()> {
+    if let Some(matches) = matches.subcommand_matches("new") {
+        let nodes: Vec<&str> = matches
+            .values_of("node")
+            .expect("at least a node must be provided")
+            .collect();
+        let mut builder =
+            manager.create_account(ClientOptionsBuilder::nodes(&nodes)?.build().unwrap());
+        if let Some(alias) = matches.value_of("alias") {
+            builder = builder.alias(alias);
+        }
+        if let Some(mnemonic) = matches.value_of("mnemonic") {
+            builder = builder.mnemonic(mnemonic);
+        }
+        let account = builder.initialise()?;
+        println!("Created account `{}`", account.alias());
+        account::account_prompt(account_cli, account);
+    }
+    Ok(())
+}
+
+fn delete_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
+    if let Some(matches) = matches.subcommand_matches("delete") {
+        let account_alias = matches.value_of("alias").unwrap();
+        if let Some(account) = manager.get_account_by_alias(account_alias) {
+            manager.remove_account(account.id().into())?;
+            println!("Account removed");
+        } else {
+            println!("Account not found");
+        }
+    }
+    Ok(())
+}
+
+fn sync_accounts_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
+    if let Some(_) = matches.subcommand_matches("sync") {
+        let synced = block_on(async move { manager.sync_accounts().await })?;
+        println!("Synchronized {} accounts", synced.len());
+    }
+    Ok(())
+}
+
+fn backup_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
+    if let Some(matches) = matches.subcommand_matches("backup") {
+        let destination = matches.value_of("path").unwrap();
+        let full_path = manager.backup(destination)?;
+        println!("Backup stored at {:?}", full_path);
+    }
+    Ok(())
+}
+
+fn import_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
+    if let Some(matches) = matches.subcommand_matches("backup") {
+        let source = matches.value_of("path").unwrap();
+        manager.import_accounts(source)?;
+        println!("Backup successfully imported");
+    }
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    let runtime = Runtime::new().expect("Failed to create async runtime");
+    RUNTIME
+        .set(Mutex::new(runtime))
+        .expect("Failed to store async runtime");
+
+    let mut manager = AccountManager::with_storage_path(
+        var_os("WALLET_DATABASE_PATH")
+            .map(|os_str| os_str.into_string().expect("invalid WALLET_DATABASE_PATH"))
+            .unwrap_or_else(|| "./wallet-cli-database".to_string()),
+    )?;
+    manager.set_stronghold_password("password")?;
+
+    let yaml = load_yaml!("account-cli.yml");
+    let account_cli = App::from(yaml)
+        .help_template(ACCOUNT_CLI_TEMPLATE)
+        .setting(AppSettings::DisableVersion)
+        .setting(AppSettings::NoBinaryName);
+
+    if std::env::args().len() == 1 {
+        let accounts = manager.get_accounts()?;
+        match accounts.len() {
+            0 => {}
+            1 => account::account_prompt(&account_cli, accounts.first().unwrap().clone()),
+            _ => pick_account(&account_cli, accounts)?,
+        }
+    }
+
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml)
+        .help_template(CLI_TEMPLATE)
+        .setting(AppSettings::ColoredHelp)
+        .get_matches();
+
+    select_account_command(&account_cli, &manager, &matches);
+    new_account_command(&account_cli, &manager, &matches)?;
+    delete_account_command(&manager, &matches)?;
+    sync_accounts_command(&manager, &matches)?;
+    backup_command(&manager, &matches)?;
+    import_command(&manager, &matches)?;
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        print_error(e);
+    }
+}
