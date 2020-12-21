9cb7851df691db3c21136f1ebad93b424a0f863f
Lucas Nogueira <lucas@tauri.studio>
2020-12-21 14:04:25 -0300

    chore(cli): update to latest wallet.rs

--- src/account.rs
+++ src/account.rs
@@ -1,23 +1,20 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{block_on, print_error};
+use crate::print_error;
 
 use clap::{App, ArgMatches};
 use dialoguer::Input;
 use iota::message::prelude::MessageId;
 use iota_wallet::{
-    account::Account,
+    account::AccountHandle,
     address::Address,
     client::ClientOptionsBuilder,
     message::{Message, MessageType, Transfer},
     Result,
 };
 
-use std::{
-    str::FromStr,
-    sync::{Arc, RwLock},
-};
+use std::{num::NonZeroU64, process::Command, str::FromStr};
 
 fn print_message(message: &Message) {
     println!("MESSAGE {}", message.id());
@@ -41,10 +38,11 @@ fn print_address(address: &Address) {
 }
 
 // `list-messages` command
-fn list_messages_command(account: &Account, matches: &ArgMatches) {
+async fn list_messages_command(account_handle: &AccountHandle, matches: &ArgMatches) {
     if let Some(matches) = matches.subcommand_matches("list-messages") {
         if let Some(id) = matches.value_of("id") {
             if let Ok(message_id) = MessageId::from_str(id) {
+                let account = account_handle.read().await;
                 if let Some(message) = account.get_message(&message_id) {
                     print_message(message);
                 } else {
@@ -54,6 +52,7 @@ fn list_messages_command(account: &Account, matches: &ArgMatches) {
                 println!("Message id must be a hex string of length 64");
             }
         } else {
+            let account = account_handle.read().await;
             let message_type = if let Some(message_type) = matches.value_of("type") {
                 match message_type {
                     "received" => Some(MessageType::Received),
@@ -78,59 +77,60 @@ fn list_messages_command(account: &Account, matches: &ArgMatches) {
 }
 
 // `list-addresses` command
-fn list_addresses_command(account: &mut Account, matches: &ArgMatches) {
+async fn list_addresses_command(account_handle: &AccountHandle, matches: &ArgMatches) {
     if matches.subcommand_matches("list-addresses").is_some() {
-        let addresses = account.addresses_mut();
+        let account = account_handle.read().await;
+        let addresses = account.addresses();
         if addresses.is_empty() {
             println!("No addresses found");
         } else {
-            addresses.sort_by_key(|a| *a.key_index());
             addresses.iter().for_each(|a| print_address(a));
         }
     }
 }
 
 // `sync` command
-fn sync_account_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn sync_account_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if matches.subcommand_matches("sync").is_some() {
-        block_on(async move { account.sync().execute().await })?;
+        account_handle.sync().await.execute().await?;
     }
     Ok(())
 }
 
 // `address` command
-fn generate_address_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn generate_address_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if matches.subcommand_matches("address").is_some() {
-        let address = account.generate_address()?;
+        let address = account_handle.generate_address().await?;
         print_address(&address);
     }
     Ok(())
 }
 
 // `balance` command
-fn balance_command(account: &Account, matches: &ArgMatches) {
+async fn balance_command(account_handle: &AccountHandle, matches: &ArgMatches) {
     if matches.subcommand_matches("balance").is_some() {
+        let account = account_handle.read().await;
         let balance = account.addresses().iter().fold(0, |acc, addr| acc + *addr.balance());
         println!("{}", balance);
     }
 }
 
 // `transfer` command
-fn transfer_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn transfer_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("transfer") {
         let address = matches.value_of("address").unwrap().to_string();
         let amount = matches.value_of("amount").unwrap();
         if let Ok(address) = iota_wallet::address::parse(address) {
             if let Ok(amount) = amount.parse::<u64>() {
-                let transfer = Transfer::new(address, amount);
-                let res: Result<()> = block_on(async move {
-                    let synced = account.sync().execute().await?;
-                    let transfer_metadata = synced.transfer(transfer).await?;
-                    *account = transfer_metadata.account;
-                    print_message(&transfer_metadata.message);
-                    Ok(())
-                });
-                res?;
+                let transfer = Transfer::builder(
+                    address,
+                    NonZeroU64::new(amount).ok_or_else(|| anyhow::anyhow!("amount can't be zero"))?,
+                )
+                .finish();
+
+                let synced = account_handle.sync().await.execute().await?;
+                let message = synced.transfer(transfer).await?;
+                print_message(&message);
             } else {
                 println!("Amount must be a number");
             }
@@ -148,19 +148,15 @@ enum ReplayAction {
 }
 
 // promotes, retries or reattaches a message
-fn replay_message(account: &mut Account, action: ReplayAction, message_id: &str) -> Result<()> {
+async fn replay_message(account_handle: &AccountHandle, action: ReplayAction, message_id: &str) -> Result<()> {
     if let Ok(message_id) = MessageId::from_str(message_id) {
-        let res: Result<()> = block_on(async move {
-            let synced = account.sync().execute().await?;
-            let message = match action {
-                ReplayAction::Promote => synced.promote(&message_id).await?,
-                ReplayAction::Retry => synced.retry(&message_id).await?,
-                ReplayAction::Reattach => synced.reattach(&message_id).await?,
-            };
-            print_message(&message);
-            Ok(())
-        });
-        res?;
+        let synced = account_handle.sync().await.execute().await?;
+        let message = match action {
+            ReplayAction::Promote => synced.promote(&message_id).await?,
+            ReplayAction::Retry => synced.retry(&message_id).await?,
+            ReplayAction::Reattach => synced.reattach(&message_id).await?,
+        };
+        print_message(&message);
     } else {
         println!("Message id must be a hex string of length 64");
     }
@@ -168,72 +164,81 @@ fn replay_message(account: &mut Account, action: ReplayAction, message_id: &str)
 }
 
 // `promote` command
-fn promote_message_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn promote_message_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("promote") {
         let message_id = matches.value_of("id").unwrap();
-        replay_message(account, ReplayAction::Promote, message_id)?;
+        replay_message(account_handle, ReplayAction::Promote, message_id).await?;
     }
     Ok(())
 }
 
 // `retry` command
-fn retry_message_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn retry_message_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("retry") {
         let message_id = matches.value_of("id").unwrap();
-        replay_message(account, ReplayAction::Retry, message_id)?;
+        replay_message(account_handle, ReplayAction::Retry, message_id).await?;
     }
     Ok(())
 }
 
 // `reattach` command
-fn reattach_message_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn reattach_message_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("reattach") {
         let message_id = matches.value_of("id").unwrap();
-        replay_message(account, ReplayAction::Reattach, message_id)?;
+        replay_message(account_handle, ReplayAction::Reattach, message_id).await?;
     }
     Ok(())
 }
 
 // `set-node` command
-fn set_node_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn set_node_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("set-node") {
         let node = matches.value_of("node").unwrap();
-        account.set_client_options(ClientOptionsBuilder::node(node)?.build());
+        account_handle
+            .set_client_options(ClientOptionsBuilder::node(node)?.build())
+            .await;
     }
     Ok(())
 }
 
 // `set-alias` command
-fn set_alias_command(account: &mut Account, matches: &ArgMatches) -> Result<()> {
+async fn set_alias_command(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("set-alias") {
         let alias = matches.value_of("alias").unwrap();
-        account.set_alias(alias);
+        account_handle.set_alias(alias).await;
     }
     Ok(())
 }
 
 // account prompt commands
-fn account_commands(account: &mut Account, matches: &ArgMatches) -> Result<()> {
-    list_messages_command(account, &matches);
-    list_addresses_command(account, &matches);
-    sync_account_command(account, &matches)?;
-    generate_address_command(account, &matches)?;
-    balance_command(account, &matches);
-    transfer_command(account, &matches)?;
-    promote_message_command(account, &matches)?;
-    retry_message_command(account, &matches)?;
-    reattach_message_command(account, &matches)?;
-    set_node_command(account, &matches)?;
-    set_alias_command(account, &matches)?;
+async fn account_commands(account_handle: &AccountHandle, matches: &ArgMatches) -> Result<()> {
+    list_messages_command(account_handle, &matches).await;
+    list_addresses_command(account_handle, &matches).await;
+    sync_account_command(account_handle, &matches).await?;
+    generate_address_command(account_handle, &matches).await?;
+    balance_command(account_handle, &matches).await;
+    transfer_command(account_handle, &matches).await?;
+    promote_message_command(account_handle, &matches).await?;
+    retry_message_command(account_handle, &matches).await?;
+    reattach_message_command(account_handle, &matches).await?;
+    set_node_command(account_handle, &matches).await?;
+    set_alias_command(account_handle, &matches).await?;
     Ok(())
 }
 
 // loop on the account prompt
-pub fn account_prompt(account_cli: &App<'_>, accounts: Arc<RwLock<Vec<Account>>>, index: usize) {
-    let alias = {
-        let accounts_ = accounts.read().unwrap();
-        accounts_[index].alias().clone()
-    };
+pub async fn account_prompt(account_cli: &App<'_>, account_handle: AccountHandle) {
+    loop {
+        let exit = account_prompt_internal(account_cli, account_handle.clone()).await;
+        if exit {
+            break;
+        }
+    }
+}
+
+// loop on the account prompt
+pub async fn account_prompt_internal(account_cli: &App<'_>, account_handle: AccountHandle) -> bool {
+    let alias = account_handle.alias().await;
     let command: String = Input::new()
         .with_prompt(format!("Account `{}` command (h for help)", alias))
         .interact_text()
@@ -245,7 +250,7 @@ pub fn account_prompt(account_cli: &App<'_>, accounts: Arc<RwLock<Vec<Account>>>
             cli.print_help().unwrap();
         }
         "clear" => {
-            let _ = std::process::Command::new("clear").status();
+            let _ = Command::new("clear").status();
         }
         _ => {
             match account_cli
@@ -254,12 +259,10 @@ pub fn account_prompt(account_cli: &App<'_>, accounts: Arc<RwLock<Vec<Account>>>
             {
                 Ok(matches) => {
                     if matches.subcommand_matches("exit").is_some() {
-                        return;
+                        return true;
                     }
 
-                    let mut accounts_ = accounts.write().unwrap();
-                    let mut account = accounts_.get_mut(index).unwrap();
-                    if let Err(e) = account_commands(&mut account, &matches) {
+                    if let Err(e) = account_commands(&account_handle, &matches).await {
                         print_error(e);
                     }
                 }
@@ -270,5 +273,5 @@ pub fn account_prompt(account_cli: &App<'_>, accounts: Arc<RwLock<Vec<Account>>>
         }
     }
 
-    account_prompt(account_cli, accounts, index)
+    false
 }
--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,13 @@ use console::Term;
 use dialoguer::{theme::ColorfulTheme, Select};
 use dotenv::dotenv;
 use iota_wallet::{
-    account::{Account, AccountIdentifier},
-    account_manager::AccountManager,
-    client::ClientOptionsBuilder,
-    signing::SignerType,
+    account::AccountHandle, account_manager::AccountManager, client::ClientOptionsBuilder, signing::SignerType,
     storage::sqlite::SqliteStorageAdapter,
-    Result, WalletError,
 };
 use once_cell::sync::OnceCell;
 use tokio::runtime::Runtime;
 
-use std::{
-    env::var_os,
-    sync::{Arc, Mutex, RwLock},
-};
+use std::{env::var_os, sync::Mutex};
 
 mod account;
 
@@ -38,39 +31,37 @@ const ACCOUNT_CLI_TEMPLATE: &str = "\
   {all-args}{after-help}\
 ";
 
-fn print_error(e: WalletError) {
+pub type Result<T> = anyhow::Result<T>;
+
+fn print_error<E: ToString>(e: E) {
     println!("ERROR: {}", e.to_string());
 }
 
 static RUNTIME: OnceCell<Mutex<Runtime>> = OnceCell::new();
 
-pub fn block_on<C: futures::Future>(cb: C) -> C::Output {
-    let runtime = RUNTIME.get().unwrap();
-    runtime.lock().unwrap().block_on(cb)
-}
-
-fn pick_account(account_cli: &App<'_>, accounts: Arc<RwLock<Vec<Account>>>) -> Result<()> {
-    let items: Vec<String> = {
-        let accounts_ = accounts.read().unwrap();
-        accounts_.iter().map(|acc| acc.alias().clone()).collect()
-    };
+async fn pick_account(accounts: Vec<AccountHandle>) -> Option<usize> {
+    let mut items = Vec::new();
+    for account_handle in accounts {
+        items.push(account_handle.alias().await);
+    }
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Select an account to manipulate")
         .items(&items)
         .default(0)
-        .interact_on_opt(&Term::stderr())?;
+        .interact_on_opt(&Term::stderr())
+        .unwrap_or_default();
     if let Some(selected) = selection {
-        account::account_prompt(account_cli, accounts.clone(), selected);
-        pick_account(account_cli, accounts)?;
+        Some(selected)
+    } else {
+        None
     }
-    Ok(())
 }
 
-fn select_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result<Option<AccountIdentifier>> {
+async fn select_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result<Option<AccountHandle>> {
     if let Some(matches) = matches.subcommand_matches("account") {
         let alias = matches.value_of("alias").unwrap();
-        if let Some(account) = manager.get_account_by_alias(alias) {
-            return Ok(Some(account.id().clone()));
+        if let Some(account) = manager.get_account_by_alias(alias).await {
+            return Ok(Some(account));
         } else {
             println!("Account not found");
         }
@@ -78,7 +69,7 @@ fn select_account_command(manager: &AccountManager, matches: &ArgMatches) -> Res
     Ok(None)
 }
 
-fn new_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result<Option<Account>> {
+async fn new_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result<Option<AccountHandle>> {
     if let Some(matches) = matches.subcommand_matches("new") {
         let nodes: Vec<&str> = matches
             .values_of("node")
@@ -98,19 +89,19 @@ fn new_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result
         if let Some(mnemonic) = matches.value_of("mnemonic") {
             builder = builder.mnemonic(mnemonic);
         }
-        let account = builder.initialise()?;
-        println!("Created account `{}`", account.alias());
+        let account = builder.initialise().await?;
+        println!("Created account `{}`", account.alias().await);
         Ok(Some(account))
     } else {
         Ok(None)
     }
 }
 
-fn delete_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
+async fn delete_account_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("delete") {
         let account_alias = matches.value_of("alias").unwrap();
-        if let Some(account) = manager.get_account_by_alias(account_alias) {
-            manager.remove_account(account.id())?;
+        if let Some(account) = manager.get_account_by_alias(account_alias).await {
+            manager.remove_account(&account.id().await).await?;
             println!("Account removed");
         } else {
             println!("Account not found");
@@ -119,9 +110,9 @@ fn delete_account_command(manager: &AccountManager, matches: &ArgMatches) -> Res
     Ok(())
 }
 
-fn sync_accounts_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
+async fn sync_accounts_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
     if matches.subcommand_matches("sync").is_some() {
-        let synced = block_on(async move { manager.sync_accounts().await })?;
+        let synced = manager.sync_accounts().await?;
         println!("Synchronized {} accounts", synced.len());
     }
     Ok(())
@@ -136,72 +127,26 @@ fn backup_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> 
     Ok(())
 }
 
-fn import_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
+async fn import_command(manager: &AccountManager, matches: &ArgMatches) -> Result<()> {
     if let Some(matches) = matches.subcommand_matches("backup") {
         let source = matches.value_of("path").unwrap();
-        manager.import_accounts(source)?;
+        manager.import_accounts(source).await?;
         println!("Backup successfully imported");
     }
     Ok(())
 }
 
-fn watch_accounts(accounts: Arc<RwLock<Vec<Account>>>) {
-    let accounts_ = accounts.clone();
-    iota_wallet::event::on_balance_change(move |event| {
-        let mut accounts_ = accounts_.write().unwrap();
-        let account = accounts_.iter_mut().find(|a| &a.id() == event.account_id()).unwrap();
-        let account_address = account
-            .addresses_mut()
-            .iter_mut()
-            .find(|a| a == event.address())
-            .unwrap();
-        account_address.set_balance(*event.balance());
-    });
-
-    let accounts_ = accounts.clone();
-    iota_wallet::event::on_new_transaction(move |event| {
-        let mut accounts_ = accounts_.write().unwrap();
-        let account = accounts_.iter_mut().find(|a| &a.id() == event.account_id()).unwrap();
-        account.append_messages(vec![event.cloned_message()]);
-    });
-
-    let accounts_ = accounts.clone();
-    iota_wallet::event::on_confirmation_state_change(move |event| {
-        let mut accounts_ = accounts_.write().unwrap();
-        let account = accounts_.iter_mut().find(|a| &a.id() == event.account_id()).unwrap();
-        if let Some(message) = account.messages_mut().iter_mut().find(|m| m == event.message()) {
-            message.set_confirmed(Some(*event.confirmed()));
-        }
-    });
-
-    let accounts_ = accounts.clone();
-    iota_wallet::event::on_reattachment(move |event| {
-        let mut accounts_ = accounts_.write().unwrap();
-        let account = accounts_.iter_mut().find(|a| &a.id() == event.account_id()).unwrap();
-        account.append_messages(vec![event.cloned_message()]);
-    });
-
-    let accounts_ = accounts;
-    iota_wallet::event::on_broadcast(move |event| {
-        let mut accounts_ = accounts_.write().unwrap();
-        let account = accounts_.iter_mut().find(|a| &a.id() == event.account_id()).unwrap();
-        if let Some(message) = account.messages_mut().iter_mut().find(|m| m == event.message()) {
-            message.set_broadcasted(true);
-        }
-    });
-}
-
-fn run() -> Result<()> {
+async fn run() -> Result<()> {
     let runtime = Runtime::new().expect("Failed to create async runtime");
     RUNTIME.set(Mutex::new(runtime)).expect("Failed to store async runtime");
 
     let storage_path = var_os("WALLET_DATABASE_PATH")
         .map(|os_str| os_str.into_string().expect("invalid WALLET_DATABASE_PATH"))
         .unwrap_or_else(|| "./wallet-cli-database".to_string());
-    let mut manager =
-        AccountManager::with_storage_adapter(&storage_path, SqliteStorageAdapter::new(&storage_path, "accounts")?)?;
-
-    manager.start_background_sync();
+    let manager = AccountManager::builder()
+        .with_storage(&storage_path, SqliteStorageAdapter::new(&storage_path, "accounts")?)
+        .finish()
+        .await?;
 
     let yaml = load_yaml!("account-cli.yml");
     let account_cli = App::from(yaml)
@@ -209,20 +154,16 @@ fn run() -> Result<()> {
         .setting(AppSettings::DisableVersion)
         .setting(AppSettings::NoBinaryName);
 
-    let accounts = manager.get_accounts()?;
-    let accounts = Arc::new(RwLock::new(accounts));
-
-    watch_accounts(accounts.clone());
-
     if std::env::args().len() == 1 {
-        let len = {
-            let a = accounts.read().unwrap();
-            a.len()
-        };
-        match len {
+        let accounts = manager.get_accounts().await;
+        match accounts.len() {
             0 => {}
-            1 => account::account_prompt(&account_cli, accounts.clone(), 0),
-            _ => pick_account(&account_cli, accounts.clone())?,
+            1 => account::account_prompt(&account_cli, accounts.first().unwrap().clone()).await,
+            _ => {
+                while let Some(index) = pick_account(accounts.clone()).await {
+                    account::account_prompt(&account_cli, accounts[index].clone()).await;
+                }
+            }
         }
     }
 
@@ -233,42 +174,34 @@ fn run() -> Result<()> {
         .setting(AppSettings::ArgRequiredElseHelp)
         .get_matches();
 
-    match select_account_command(&manager, &matches) {
-        Ok(Some(selected_account_id)) => {
-            let index = {
-                let accounts_ = accounts.read().unwrap();
-                accounts_.iter().position(|a| a.id() == &selected_account_id).unwrap()
-            };
-            account::account_prompt(&account_cli, accounts.clone(), index);
+    match select_account_command(&manager, &matches).await {
+        Ok(Some(account)) => {
+            account::account_prompt(&account_cli, account).await;
         }
         Ok(None) => {}
         Err(e) => return Err(e),
     };
-    match new_account_command(&manager, &matches) {
-        Ok(Some(new_account)) => {
-            let index = {
-                let mut accounts_ = accounts.write().unwrap();
-                accounts_.push(new_account);
-                accounts_.len() - 1
-            };
-            account::account_prompt(&account_cli, accounts, index);
+    match new_account_command(&manager, &matches).await {
+        Ok(Some(new_account_handle)) => {
+            account::account_prompt(&account_cli, new_account_handle).await;
         }
         Ok(None) => {}
         Err(e) => return Err(e),
     };
-    delete_account_command(&manager, &matches)?;
-    sync_accounts_command(&manager, &matches)?;
+    delete_account_command(&manager, &matches).await?;
+    sync_accounts_command(&manager, &matches).await?;
     backup_command(&manager, &matches)?;
-    import_command(&manager, &matches)?;
+    import_command(&manager, &matches).await?;
 
     Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     if let Ok(p) = dotenv() {
         println!("loaded dotenv from {:?}", p);
     }
-    if let Err(e) = run() {
+    if let Err(e) = run().await {
         print_error(e);
     }
 }
