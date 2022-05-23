656d756a6ab0cd9dce8151e8226758743e949881
Thibault Martinez <thibault@iota.org>
2022-05-23 18:21:05 +0200

    Simplifies match

--- src/account_manager.rs
+++ src/account_manager.rs
@@ -16,22 +16,10 @@ pub async fn match_account_manager_command(
     account_manager_cli: AccountManagerCli,
 ) -> Result<(), Error> {
     match account_manager_cli.command {
-        AccountManagerCommand::Init(mnemonic_url) => {
-            init_command(account_manager, mnemonic_url).await?;
-        }
-        AccountManagerCommand::New { alias } => {
-            new_command(account_manager, alias).await?;
-        }
-        AccountManagerCommand::Select { identifier } => {
-            select_command(account_manager, identifier).await?;
-        }
-        AccountManagerCommand::SetNode { url } => {
-            set_node_command(account_manager, url).await?;
-        }
-        AccountManagerCommand::Sync => {
-            sync_command(account_manager).await?;
-        }
+        AccountManagerCommand::Init(mnemonic_url) => init_command(account_manager, mnemonic_url).await,
+        AccountManagerCommand::New { alias } => new_command(account_manager, alias).await,
+        AccountManagerCommand::Select { identifier } => select_command(account_manager, identifier).await,
+        AccountManagerCommand::SetNode { url } => set_node_command(account_manager, url).await,
+        AccountManagerCommand::Sync => sync_command(account_manager).await,
     }
-
-    Ok(())
 }
